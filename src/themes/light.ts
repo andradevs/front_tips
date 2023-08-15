0827211b096be2e40492ace67017ab437e2a417c
andradevs <guilhermeandradevs@gmail.com>
2023-08-15 12:19:53 -0300

    setting theme colors

--- src/themes/light.ts
+++ src/themes/light.ts
@@ -1,1 +1,14 @@
-export const light = {};
+export const light = {
+  primary: '#5f04ee',
+  primary_variant: '',
+  secondary: '',
+  secondary_variant: '',
+  background: '',
+  suraface: '',
+  error: '',
+  onPrimary: '',
+  onSecondary: '',
+  onBackground: '',
+  onSurface: '',
+  onError: '',
+};
